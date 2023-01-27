fa54d63107ee80e172b7380adb7f6980cbabd87f
Nabila Adawy <nabilahosny57@gmail.com>
2023-01-27 14:19:07 +0300

    minor edits

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
 import numpy as np
 import sympy as sp
 from casadi import *
-import casadi as ca
-from sympy.utilities.lambdify import lambdify
-from numpy import genfromtxt
 from lqr_gain import dLQR
 from jacobians import num_jacobian
-from dynamics import dynamics
 from plotting import plotting
 from constraints import constraints
 from objective import objective_func
@@ -16,34 +12,34 @@ from simulation import simulation
 
 """
 Trajectory Optimization Using Casadi Framework.
-"""                 
+"""
 
 # Numerical parameters
 I1, m1, M1, L1, g1 = 0.006, 0.3, 1, 0.5, 9.81
 num_param = I1, m1, M1, L1, g1
-d_max = 2 # track half length
-d = 1 # desired cart distance
-u_max = 25 # motor peak force 
+d_max = 2  # track half length
+d = 1  # desired cart distance
+u_max = 25  # motor peak force
 x_0 = [0, 0, 0, 0]  # Initial state
-x_f = [d, np.pi, 0,0] # final state
-Q_casa, R_casa  = 1, 40 # gains for the objective function
+x_f = [d, np.pi, 0, 0]  # final state
+Q_casa, R_casa = 1, 40  # gains for the objective function
 
 # Simulation Parameters
-N = 200 # opt trajectory points
-T = 2 # total time
-dT = T/N 
+N = 200  # opt trajectory points
+T = 2  # total time
+dT = T/N
 t1 = np.linspace(0, T, N)
 
 # Define NLP problem
-problem = Opti() 
+problem = Opti()
 
 # Decision variables
-state = problem.variable(4,N+1) # state of the system
-q1 = state[0,:] # cart distance 
-q2 = state[1,:] # pole angle
-dq1 = state[2,:] # cart horizontal velocity 
-dq2 = state[3,:] # pole angular velocity
-u = problem.variable(1,N)   # control variables (motor force)
+state = problem.variable(4, N+1)  # state of the system
+q1 = state[0, :]  # cart distance
+q2 = state[1, :]  # pole angle
+dq1 = state[2, :]  # cart horizontal velocity
+dq2 = state[3, :]  # pole angular velocity
+u = problem.variable(1, N)   # control variables (motor force)
 
 # Problem constraints
 constraints(problem, u_max, u, d_max, q1, q2, dq1, dq2, x_0, x_f)
@@ -58,7 +54,7 @@ trapz_colloc(problem, u, state, N, dT, num_param)
 init_guess(problem, N, u, q1, q2, dq1, dq2, x_0, x_f)
 
 # solve NLP
-problem.solver("ipopt") # set numerical backend
+problem.solver("ipopt")  # set numerical backend
 sol = problem.solve()   # solve defined problem
 
 # parsing the solution from sol
@@ -81,30 +77,31 @@ sym_param = I, m, M, L, g
 x_sym = sp.symbols(r'\theta \dot{\theta} x \dot{x}')
 u_sym = sp.symbols(r'u')
 
-C = np.array([[1, 0, 0 ,0]])
+C = np.array([[1, 0, 0, 0]])
 D = np.array([[0, 0]])
 Q = np.diag([12.5, 12.5, 12.5, 12.5])
 R = np.diag([0.95])
 
 # Evaluating the numerical jacobians at N points of the desired trajectory
-A_num, B_num = num_jacobian(x_sym, u_sym, sym_param, x_d, u_opt, num_param, N, C, D, dT, 'var')
+A_num, B_num = num_jacobian(x_sym, u_sym, sym_param,
+                            x_d, u_opt, num_param, N, C, D, dT, 'var')
 
 # Compute the LQR gain K
 K_vt = dLQR(A_num, B_num, Q, Q, R, 'tv')
 
-# For Stabilization 
+# For Stabilization
 N2 = 1000
 T2 = 10
 t2 = np.linspace(0, T2, N2)
 
-xd = np.zeros((4,N2))
+xd = np.zeros((4, N2))
 ud = np.zeros(N2)
-Kd = np.zeros((N2,4))
+Kd = np.zeros((N2, 4))
 
 # Simulating the system
-X, U = simulation(N, N2, dT, Q, R, C, D, x_0, xd, ud, Kd, x_d, u_opt, K_vt, x_sym, u_sym, sym_param, num_param)
-theta, dtheta, pos, dpos = X[:,0], X[:,1], X[:,2], X[:,3] 
+X, U = simulation(N, N2, dT, Q, R, C, D, x_0, xd, ud, Kd, x_d,
+                  u_opt, K_vt, x_sym, u_sym, sym_param, num_param)
+theta, dtheta, pos, dpos = X[:, 0], X[:, 1], X[:, 2], X[:, 3]
 
 # Plotting the optimal and actual trajectories
 plotting(t1, t2, pos, dist, theta, ang, U, u_opt)
-
--- plotting.py
+++ plotting.py
@@ -1,11 +1,12 @@
 import numpy as np
 from matplotlib.pyplot import *
 
+
 def plotting(t1, t2, p, dist, theta, ang, U, u_opt):
     figure(figsize=(25, 8))
-    plot(t2, p , linewidth=2.0, label='distance')
-    plot(t1, dist[:-1],'r', linewidth=2.0, label='opt_distance')
-    grid(color='black', linestyle='--', linewidth=1.0, alpha = 0.7)
+    plot(t2, p, linewidth=2.0, label='distance')
+    plot(t1, dist, 'r', linewidth=2.0, label='opt_distance')
+    grid(color='black', linestyle='--', linewidth=1.0, alpha=0.7)
     grid(True)
     xlabel(r'Time  $T$ (min)')
     legend()
@@ -13,8 +14,8 @@ def plotting(t1, t2, p, dist, theta, ang, U, u_opt):
 
     figure(figsize=(25, 8))
     plot(t2, theta, linewidth=2.0, label='angle')
-    plot(t1, ang[:-1],'r', linewidth=2.0, label='opt_angle')
-    grid(color='black', linestyle='--', linewidth=1.0, alpha = 0.7)
+    plot(t1, ang, 'r', linewidth=2.0, label='opt_angle')
+    grid(color='black', linestyle='--', linewidth=1.0, alpha=0.7)
     grid(True)
     xlabel(r'Time  $T$ (min)')
     legend()
@@ -22,11 +23,10 @@ def plotting(t1, t2, p, dist, theta, ang, U, u_opt):
 
     figure(figsize=(25, 8))
     plot(t2, U, label='control')
-    plot(t1, u_opt,'r', linewidth=2.0, label='opt_control')
-    grid(color='black', linestyle='--', linewidth=1.0, alpha = 0.7)
+    plot(t1[:-1], u_opt, 'r', linewidth=2.0, label='opt_control')
+    grid(color='black', linestyle='--', linewidth=1.0, alpha=0.7)
     grid(True)
     ylabel(r'Control $\mathbf{u}[k]$')
     xlabel(r'Time  $T$ (min)')
     legend()
     show()
-
